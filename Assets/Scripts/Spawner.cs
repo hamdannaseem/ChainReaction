5d1ab243239d8644c7618008aebac9ba15319670
Hamdan Naseem <hamdunaseem@gmail.com>
2025-08-23 15:46:52 +0530

    Spawn functionality Implemented

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,16 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float SmallSize, BigSize, DropHeight, ArenaLimits;
+    public Material[] Materials;
+    public GameObject[] Prefabs;
+    float SpawnSize, RandomSize;
+    Stack<GameObject> pool = new Stack<GameObject>();
+    public void SetSize(bool Big)
     {
-        
+        if (Big)
+        {
+            SpawnSize = BigSize;
+        }
+        else
+        {
+            SpawnSize = SmallSize;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetRandomSize()
     {
-        
+        if (Random.Range(BigSize, SmallSize) > ((BigSize + SmallSize) / 2))
+        {
+            SpawnSize = SmallSize;
+        }
+        else
+        {
+            SpawnSize = BigSize;
+        }
+    }
+    public void Spawn(int PrefabIndex)
+    {
+        if (PrefabIndex > Prefabs.Length) { return; }
+        Vector3 SpawnPos = new Vector3(Random.Range(ArenaLimits, -ArenaLimits), DropHeight, Random.Range(ArenaLimits, -ArenaLimits));
+        GameObject spawnedPrefab = Instantiate(Prefabs[PrefabIndex], SpawnPos, Quaternion.identity);
+        spawnedPrefab.GetComponent<Renderer>().material = Materials[Random.Range(0, Materials.Length)];
+        spawnedPrefab.transform.localScale = spawnedPrefab.transform.localScale * SpawnSize;
+        pool.Push(spawnedPrefab);
+    }
+    public void Spawn()
+    {
+        Spawn(Random.Range(0, Prefabs.Length));
+    }
+    public void Remove()
+    {
+        if (pool.Count == 0) { return; }
+        Destroy(pool.Peek());
+        pool.Pop();
     }
 }
